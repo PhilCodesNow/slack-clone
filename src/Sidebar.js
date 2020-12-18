7ef0e710b3ac96c7f807842ea9fc2f8582d679d0
PhilCodesNow <philipmayojr@outlook.com>
2020-12-17 19:57:11 -0500

    working on sidebar options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Sidebar.css';
 import FiberManualRecord from "@material-ui/icons/FiberManualRecord";
 import Create from "@material-ui/icons/Create";
+import SidebarIcon from "./SidebarOption";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
 
 function Sidebar() {
     return (
-        <div classname="sidebar">
+        <div className="sidebar">
             <div className="sidebar__header">
                 <div className="sidebar__info">
                     <h2>Clever Programmer</h2>
@@ -14,7 +16,12 @@ function Sidebar() {
                         Rafeh Qazi
                     </h3>
                 </div>
+                    <Create/>
+                    
+                
             </div>
+                <SidebarIcon Icon={InsertCommentIcon} title="Threads"/>
+                <SidebarIcon title="youtube" />
         </div>
     )
 }
