91d869cb292b45c979d3c9fc01915fb60bd6a48d
PhilCodesNow <philipmayojr@outlook.com>
2020-12-19 07:35:13 -0500

    most of sidebar styling done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,15 @@ import FiberManualRecord from "@material-ui/icons/FiberManualRecord";
 import Create from "@material-ui/icons/Create";
 import SidebarIcon from "./SidebarOption";
 import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy"
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
     return (
@@ -21,7 +30,19 @@ function Sidebar() {
                 
             </div>
                 <SidebarIcon Icon={InsertCommentIcon} title="Threads"/>
-                <SidebarIcon title="youtube" />
+                <SidebarIcon Icon={InboxIcon} title="Mentions"/>
+                <SidebarIcon Icon={DraftsIcon} title="Saved"/>
+                <SidebarIcon Icon={BookmarkBorderIcon} title="Channel Browser"/>
+                <SidebarIcon Icon={PeopleAltIcon} title="People"/>
+                <SidebarIcon Icon={AppsIcon} title="Apps"/>
+                <SidebarIcon Icon={FileCopyIcon} title="File Browser"/>
+                <SidebarIcon Icon={ExpandLessIcon} title="Show Less"/>
+                <hr/>
+                <SidebarIcon Icon={ExpandMoreIcon} title="Channels"/>
+                <hr/>
+                <SidebarIcon Icon={AddIcon} addChannelOptions title="Add Chanel"/>
+                {/* Connect to db and list all channels */}
+
         </div>
     )
 }
